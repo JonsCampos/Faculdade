054d10d140fc5111d24ef5eabb5176d6c3185cc5
Jonas Campos <159673319+JonsCampos@users.noreply.github.com>
2025-04-26 16:58:32 -0300

    Add files via upload

---
+++ Desenv. Web em HTML5, CSS, JAVASCRIPT e PHP/index.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+Criacao de um crud para cadastro de alunos com suas respectivas nota final e disciplina.
+tabela:
+
+id auto incremento
+nome text
+matricula text
+nota_final numeric
+disciplina text
+data_criacao time
+
+*/
+?>
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+
+      <a href="Aluno/list-aluno.php"> Exibir Alunos</a>
+  </body>
+</html>
+
