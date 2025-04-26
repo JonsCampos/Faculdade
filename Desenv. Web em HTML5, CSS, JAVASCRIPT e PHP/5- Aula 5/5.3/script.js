eeccc0e50d408161790644d3ff3a823d0f9617df
Jonas Campos <159673319+JonsCampos@users.noreply.github.com>
2025-04-26 15:44:07 -0300

    Aula5.3

---
+++ Desenv. Web em HTML5, CSS, JAVASCRIPT e PHP/5- Aula 5/5.3/script.js
@@ -0,0 +1,18 @@
+array = ['João','2018','Belo Horizonte'];
+console.log(array);//joao
+
+array2 = [ 
+  ['Renato','2018','Belo Horizonte'],
+  ['Maria','2019','Acre']
+          ];
+
+console.log(array2[0]); //Renato
+console.log(array2[1]); //Maria
+console.log(array2[1][1]); //Maria
+
+json = { "nome": "João",
+        "ano":2018,
+        "cidade": "Belo Horizonte"
+       };
+
+console.log(json);
