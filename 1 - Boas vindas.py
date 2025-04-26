7d87a0cd16314c8a0ec29bdce3840912eb9a8a89
Jonas Campos <159673319+JonsCampos@users.noreply.github.com>
2025-04-26 17:29:01 -0300

    Add files via upload

---
+++ 1 - Boas vindas.py
@@ -0,0 +1,4 @@
+nome=input('Digite o seu nome: ')
+print(f'Bem vindo {nome} a sua primeira aula de Python')
+idade=int(input('Digite sua idade: '))
+print(f'O(A) aluno(a) {nome} possui {idade} anos')
